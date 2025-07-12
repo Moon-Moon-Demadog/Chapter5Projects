ab843370fb09aacab284e4a7a18e86f45f85e2bb
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-07-12 15:25:59 +0000

    error
    something happened in my replit

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,31 +1,41 @@
-// This program will display two patterns
+//This program will play a guessing game with the user
 #include <iostream>
-#include <string>
+#include <random>
 using namespace std;
 
-int main()
+int main () 
 {
-  string pattern_char = "+";
-  string current_pattern = "";
-  int i = 0;
-  cout<< "Enjoy this pattern!"<< endl;
-
-  while (i <= 10) 
-  {
-    ++i;
-    current_pattern += pattern_char;
-    cout<< current_pattern;
-  }
-cout<< "\n";
-cout<< "Next Pattern"<< endl;
+  const int MIN = 0;
+  const int MAX = 100;
+  int number;
+  int counter = 0;
+  random_device engine;
+  uniform_int_distribution<int>RandomValue(MIN,MAX);
+  int random = RandomValue(engine);
   
-  while (i>= 10)
+  cout<< "Let's play a game!!"<< endl;
+  cout<< "\n";
+  cout<<"Guess the random number I'm thinking:"<< endl;
+  cout<< "    "<<endl;
+  cout<< endl;
+  cin>> number;
+  while (number != random)
+    {
+      if (number > random)
       {
-        --i;
-        current_pattern += pattern_char;
-        cout<< current_pattern;
+        cout<< "Too high, try again!"<< endl;
       }
-cout<< "\n";
-cout<< "End of program!";
-    return 0;
+      else if ( number < random) 
+      {
+        cout<< "Too low. try again!"<< endl;
+      }   
+      cout<< "\t";
+      cin>> number;
+      counter++;
+    }
+    
+    cout<< "Great job!! It only took you " << counter;
+    cout<< " guesses."<< endl;
+    
+  return 0;
 }
